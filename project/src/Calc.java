1a1ea081d7cbb745a0486dbd02c4ed0175581b37
Stas M <st@st.com>
2021-06-22 18:26:32 +0300

    Калькулятор

---
+++ project/src/Calc.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) throws IOException {
+        double result;
+        char operation;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Просто калькулятор");
+        System.out.println();
+        System.out.println("Введите первое число: ");
+        double a = scanner.nextDouble();
+
+        System.out.println("Введите второе число: ");
+        double b = scanner.nextDouble();
+
+        System.out.println("Введите операцию: ");
+        operation = (char) System.in.read();
+
+        switch(operation){
+            case '+':
+                result = a + b;
+                System.out.println(result);
+                break;
+            case '-':
+                result = a - b;
+                System.out.println(result);
+                break;
+            case '/':
+                result = a / b;
+                System.out.println(result);
+                break;
+            case '*':
+                result = a * b;
+                System.out.println(result);
+                break;
+
+        }
+
+    }
+}

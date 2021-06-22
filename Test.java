9bc8efbf24708bca58ccf48b2588dbe913108d09
Stas M <st@st.com>
2021-06-22 15:51:03 +0300

    My initial commit ver one

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+
+  public int a = 10;
+  public int b = 20;
+
+}
---
+++ Test2.java
@@ -0,0 +1,7 @@
+public class Test2 {
+    public double ttt(Test test){
+        double z  = test.a* test.b;
+        return z;
+    }
+
+}
